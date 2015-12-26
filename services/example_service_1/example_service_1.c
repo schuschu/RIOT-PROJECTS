4b42e46998ab65b5795b015ec78b14ca05757423
Alex V. Melnikov <avmelnikoff@gmail.com>
2015-12-26 18:34:12 +0300

    Added initial project files

---
+++ services/example_service_1/example_service_1.c
@@ -0,0 +1,9 @@
+
+
+#include <stdio.h>
+
+int example_service_1_routine(void)
+{
+    printf("example_service_1_routine: Hello World!!!\n");
+    return 0;
+}
---
+++ services/example_service_1/include/example_service_1.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2015 Alexander Melnikov
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+ 
+#ifndef EXAMPLE_SERVICE_1_H_
+#define EXAMPLE_SERVICE_1_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int example_service_1_routine(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* EXAMPLE_SERVICE_1_H_ */
