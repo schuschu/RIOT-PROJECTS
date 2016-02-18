5e402c8e66ec756d240a2c8d6062372f0a2117ae
Alex V. Melnikov <avmelnikoff@gmail.com>
2016-02-19 00:22:17 +0300

    lctech-stm32f103c8t6 board cloned to boiler-room-board for experiments

---
+++ boards/boiler-room-board/board.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2015 Alexander Melnikov <avmelnikoff@gmail.com>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser General
+ * Public License v2.1. See the file LICENSE in the top level directory for more
+ * details.
+ */
+
+/**
+ * @ingroup     boards_lctech-stm32f103c8t6
+ * @{
+ *
+ * @file
+ * @brief       Board specific implementations for the lctech-stm32f103c8t6 board
+ *
+ * @author      Alexander Melnikov <avmelnikoff@gmail.com>
+ *
+ * @}
+ */
+
+#include "board.h"
+#include "cpu.h"
+
+
+void board_init(void)
+{
+    /* initialize the CPU */
+    cpu_init();
+}
