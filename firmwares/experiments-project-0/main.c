d4acd19e34642f572545557a0f5021d7dbb36b72
Alexander V. Melnikov <avmelnikoff@gmail.com>
2016-06-26 02:40:25 +0300

    added experiments with dht driver

--- firmwares/experiments-project-0/main.c
+++ firmwares/experiments-project-0/main.c
@@ -22,9 +22,15 @@
 
 #include <periph/gpio.h>
 
+#include <xtimer.h>
+#include <dht.h>
+#include <dht_params.h>
+
 #include <board.h>
 
 #include <example_service_1.h>
+
+extern dht_t dht_devs[];
 
 int main(void)
 {
@@ -39,6 +45,37 @@ int main(void)
   printf("This board features a(n) %s MCU.\n", RIOT_MCU);
 
   example_service_1_routine();
+
+
+  puts("DHT temperature and humidity sensor test application\n");
+
+  /* periodically read temp and humidity values */
+  while (1) {
+      for (unsigned i = 0; i < DHT_NUMOF; i++) {
+          dht_t *dev = &dht_devs[i];
+          int16_t temp, hum;
+          int16_t dec_temp, dec_hum, int_temp, int_hum;
+
+          if (dht_read(dev, &temp, &hum) == -1) {
+              puts("error reading data");
+              continue;
+          }
+
+          /* split up values into integral and fractional parts for nicer
+           * printing
+           * TODO: this should be done in some kind of library... */
+          int_temp = temp / 10;
+          dec_temp = temp  - (int_temp * 10);
+          int_hum = hum / 10;
+          dec_hum = hum - (int_hum * 10);
+
+          printf("DHT device #%i - ", i);
+          printf("temp: %i.%i°C, ", int_temp, dec_temp);
+          printf("relative humidity: %i.%i%%\n", int_hum, dec_hum);
+
+          xtimer_usleep(2000 * MS_IN_USEC);
+      }
+  }
 
   return 0;
 }
