8f7afe9721fd67e84cb7e8a11da19b7ef9d0369a
Alexander V. Melnikov <avmelnikoff@gmail.com>
2016-06-26 02:02:57 +0300

    added project for experiments

---
+++ firmwares/experiments-project-0/main.c
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2015 Alexander Melnikov
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       Hello World application
+ *
+ * @author      Alexander Melnikov <avmelnikoff@gmail.com>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+
+#include <periph/gpio.h>
+
+#include <board.h>
+
+#include <example_service_1.h>
+
+int main(void)
+{
+  gpio_init(LED_GREEN_GPIO, GPIO_OUT);
+  gpio_init(LED_YELLOW_GPIO, GPIO_OUT);
+
+  gpio_set(LED_GREEN_GPIO);
+
+  puts("Hello World!");
+
+  printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
+  printf("This board features a(n) %s MCU.\n", RIOT_MCU);
+
+  example_service_1_routine();
+
+  return 0;
+}
